387da24c5b6f0d83b2dfdd6a15bedd234acff308
LorenaAgustinaLujan <lujan.agustina@hotmail.com>
2023-03-09 01:05:59 -0300

    Cheackbox js

---
+++ js/Index.js
@@ -0,0 +1,45 @@
+function displayAllEvents(data) {
+  let indexEvents = "";
+  for (let event of data.events) {
+    indexEvents += createcard(event);
+  }
+  document.getElementById("cards").innerHTML = indexEvents;
+}
+
+console.log([document])
+function crearCheckbox() {
+  const todasLasCategorias = data.events.map(evento => evento.category);
+  const categoriasUnicas = [...new Set(todasLasCategorias)];
+  const inputCheckbox = categoriasUnicas.map(category => {
+    return `<div class="form-check form-check-inline">
+               <input class="form-check-input" type="checkbox" id="${category}" value="${category}">
+               <label class="form-check-label" for="${category}">${category}</label>
+             </div>`;
+
+  }).join('');
+  document.getElementById("checkboxes").innerHTML = inputCheckbox;
+
+
+  categoriasUnicas.forEach(category => {
+    document.getElementById(category).addEventListener("change", () => {
+      const checkedBoxes = categoriasUnicas.filter(category => document.getElementById(category).checked);
+      let filteredEvents = [];
+      if (checkedBoxes.length === 0) {
+        filteredEvents = data.events;
+      } else {
+        filteredEvents = data.events.filter(event => checkedBoxes.includes(event.category));
+      }
+      displayAllEvents({ events: filteredEvents });
+    });
+  });
+
+  const searchInput = document.getElementById("search");
+  searchInput.addEventListener("keyup", () => {
+    const filteredEvents = data.events.filter(event => event.name.toLowerCase().includes(searchInput.value.trim().toLowerCase()));
+    displayAllEvents({ events: filteredEvents });
+  });
+}
+crearCheckbox();
+displayAllEvents(data);
+
+
